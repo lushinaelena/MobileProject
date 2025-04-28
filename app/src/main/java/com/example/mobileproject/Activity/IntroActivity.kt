323b3148ac7b045344b5cae1cd1ab3cd46c67272
lushinaelena <feyymr@gmail.com>
2025-04-28 20:03:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mobileproject/Activity/IntroActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mobileproject.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobileproject.databinding.ActivityIntroBinding
+import com.example.mobileproject.MainActivity
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
+    }
+}
