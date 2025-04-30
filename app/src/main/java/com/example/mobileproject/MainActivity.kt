591ff9bc36c3ce20a000db83c5ad1a93acfcb9e1
lushinaelena <feyymr@gmail.com>
2025-04-30 19:40:32 +0300

    работа с второй страницей

--- app/src/main/java/com/example/mobileproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileproject/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.mobileproject
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mobileproject.Adapter.CategoryAdapter
+import com.example.mobileproject.ViewModel.MainViewModel
+import com.example.mobileproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel = MainViewModel()
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initCategories()
         }
+
+    private fun initCategories() {
+        binding.progressBarCategory.visibility=View.VISIBLE
+        viewModel.category.observe(this, Observer {
+            binding.viewCategory.layoutManager =
+                LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+            binding.viewCategory.adapter=CategoryAdapter(it)
+            binding.progressBarCategory.visibility = View.GONE
+        })
     }
 }
